02aafb645bedecffab63dfb7119b74622033a7de
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-19 11:31:46 -0600

    Add newgame request test

---
+++ spec/requests/api/v1/newgame_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "when a client request a newgame" do
+  describe "with valid ship locations" do
+    it "responds with a game_id" do
+      ship_locations = {
+        'carrier': 'a1,a2,a3,a4,a5',
+        'battleship': 'b1,b2,b3,b4',
+        'submarine': 'c1,c2,c3',
+        'cruiser': 'd1,d2,d3',
+        'destroyer': 'e1,e2'
+      }
+      post "/api/v1/newgame", params: ship_locations
+      expect(response).to be_successful
+
+      game_response = JSON.parse(response.body, symbolize_names: true)
+
+      
+
+    end
+
+    it "responds with an api_move if the api goes first" do
+
+    end
+  end
+
+end
