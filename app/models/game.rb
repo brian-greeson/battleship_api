a9f6f8831ad22cbbeadc0c52b5e5938ce922cd65
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-19 16:14:03 -0600

    Update create action in game controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,18 @@
 class Game < ApplicationRecord
+  after_create :create_boards, :start_game
   has_one :client_board, class_name: 'Board', dependent: :destroy
   has_one :api_board, class_name: 'Board', dependent: :destroy
 
   has_secure_token :game_token
+
+  private
+
+  def create_boards
+    self.api_board = Board.create(board_size: 5)
+    self.client_board = Board.create(board_size: 5)
+  end
+
+  def start_game
+    self.status = 'waiting for client'
+  end
 end
