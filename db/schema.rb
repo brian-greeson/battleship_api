94a66fd6f3b40382f8bcbb5a49333f1afd5927fe
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-19 12:50:50 -0600

    Add game table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_19_183744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "status"
+    t.string "game_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_token"], name: "index_games_on_game_token", unique: true
+  end
 
 end
