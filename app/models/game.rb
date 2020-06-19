96b4125742b616de25042d0ca61625f78e7cb442
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-19 14:53:41 -0600

    Add board model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,6 @@
 class Game < ApplicationRecord
-  has_many :game_boards
-  has_one :client_board, through: :game_boards
-  has_one :api_board, through: :game_boards
-  
+  has_one :client_board, class_name: 'Board', dependent: :destroy
+  has_one :api_board, class_name: 'Board', dependent: :destroy
+
   has_secure_token :game_token
 end
