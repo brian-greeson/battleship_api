05cb35e2d2e805c664ff6bb1a5d8e49edf13ebb3
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-06-19 14:54:03 -0600

    Add releationship testing

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   describe "relationships" do
-    it {should have_many(baords)}
+    it { should have_one(:client_board).dependent(:destroy) }
+    it { should have_one(:api_board).dependent(:destroy) }
   end
 end
